350c137a33370822fc4a82b5b9a57e394b7aec2c
pedrocfrazao <pedrocfrazao@gmail.com>
2016-09-10 16:23:44 -0300

    #ADDs Exception
    …

--- src/Client.java
+++ src/Client.java
@@ -10,21 +10,21 @@ public class Client {
     }
 
     public static void main(String[] args) {
-        String host;
-
-        if (args.length < 1){
-            host = null;
-        }else{
-            host = args[0];
-        }
+        
+		String host = (args.length < 1) ? null : args[0];
 
         try {
+	
             Registry registry = LocateRegistry.getRegistry(host);
             Server stub = (Server) registry.lookup("ServerInterface");
             Double response = stub.sum(2,3);
 
             System.out.println("Sum: " + response);
+
         }catch (Exception e){
+			
+			System.err.println("Client exception: " + e.toString());
+			            e.printStackTrace();
 
         }
     }
