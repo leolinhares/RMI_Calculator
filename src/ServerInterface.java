ae0167961d2e84274608705ef56d9fe89e8c554b
Leo Linhares <slinharesleo@gmail.com>
2016-09-10 15:01:39 -0300

    remote interface added

--- src/ServerInterface.java
+++ src/ServerInterface.java
@@ -1,5 +1,12 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by leolinhares on 10/09/2016.
  */
-public class ServerInterface {
+public interface ServerInterface extends Remote{
+    public double sum(double a, double b) throws RemoteException;
+    public double minus(double a, double b) throws RemoteException;
+    public double times(double a, double b) throws RemoteException;
+    public double divide(double a, double b) throws RemoteException;
 }
